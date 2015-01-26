12ffad37dc345d5936aec5b9d5b823753bce2f15
Andrew Sygyda <andrew.sygyda@gmail.com>
2015-01-26 12:10:39 +0200

    make themes more mobile

--- client/views/themes/themeItem.js
+++ client/views/themes/themeItem.js
@@ -1,8 +1,11 @@
+'use strict';
+
 Template.themeItem.created = function () {
+    this.data.pageNameReactive.set(this.data.theme.name);
     setupPagination(this);
     simplePagination(this, { themeId: this.data.theme._id });
-}
+};
 
 Template.themeItem.destroyed = function () {
     $(window).off('scroll');
-}
+};
