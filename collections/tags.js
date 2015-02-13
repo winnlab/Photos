b8bb4515d788fa02d70823b24cbd89d30a6a3013
Andrew Sygyda <andrew.sygyda@gmail.com>
2015-02-13 16:06:21 +0200

    tags collection update method

--- collections/tags.js
+++ collections/tags.js
@@ -2,15 +2,16 @@ Tags = new Mongo.Collection('tags');
 
 Meteor.methods({
     addTags: function (tags) {
-        if (!tags) {
+        if (!tags || tags === ' ') {
             return;
         }
         _.each(tags, function (tag) {
             check(tag, String);
+            tag = tag.toLowerCase().replace(/\n|,| /g, '');
             var exist = Tags.findOne({ name: tag });
             if (!exist) {
                 Tags.insert({ name: tag });
             }
         });
     }
-})
+});
